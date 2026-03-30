6f968dec48ef15fa8dc20ac3dbae8b68f1912200
Venkat14122003 <146362479+Venkat14122003@users.noreply.github.com>
2026-03-30 22:55:34 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,12 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 8080,
+    port: process.env.PORT || 8080,
     allowedHosts: 'all'
   },
   preview: {
     host: '0.0.0.0',
-    port: 8080,
+    port: process.env.PORT || 8080,
     allowedHosts: 'all'
   }
 })
